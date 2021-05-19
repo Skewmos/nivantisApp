e606586f6295e3869403722ff0b4f3d8298b7a3c
Jérémie Legrand <jeremielegrand021@gmail.com>
2021-05-19 15:02:53 +0200

    [HOME] adding route and home page

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,6 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
-
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
 
@@ -30,41 +26,27 @@ import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
+/* Component */ 
+import Visualisation from './pages/visualisation';
+import Home from './pages/home';
+
 /* Theme variables */
 import './theme/variables.css';
+import './theme/theme.css';
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
-          <Route exact path="/tab1">
-            <Tab1 />
+          <Route exact path="/" >
+            <Home />
           </Route>
-          <Route exact path="/tab2">
-            <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
-          </Route>
-          <Route exact path="/">
-            <Redirect to="/tab1" />
+          <Route exact path="/visualisation">
+            <Visualisation />
           </Route>
         </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
+        <IonTabBar></IonTabBar>
       </IonTabs>
     </IonReactRouter>
   </IonApp>
--- src/Medicaments.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Medicaments from './Medicaments';
-
-test('renders without crashing', () => {
-  const { baseElement } = render(<Medicaments />);
-  expect(baseElement).toBeDefined();
-});
